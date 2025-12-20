bbc90af7145fa28de7ffd8a26c9fb3f35651293c
swapnil0076 <swapnildhiman910@gmail.com>
2025-12-20 19:05:45 +0530

    Saturday

---
+++ src/Data.js
@@ -0,0 +1,25 @@
+const sampleData = [{
+    
+      id: 1,
+      name: "John Doe",
+      age: 30,
+      email: "JhoneDoe@gmail.com"
+  },
+  {
+    
+      id: 2,
+      name: "Bill Gates",
+      age: 38,
+      email: "BillGates@gmail.com"
+  },
+  {
+      
+        id: 3,
+        name: "Dominic Smith",
+        age: 48,
+        email: "DominicSmith@gmail.com"
+    },
+
+ ];
+
+  module.exports = {sampleData};
