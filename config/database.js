1fbaec7bc32e1200bf810daf70aeced3b7f97260
swapnil0076 <swapnildhiman910@gmail.com>
2025-12-29 01:31:24 +0530

    User Scheme and Mongodb setup

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongodbUrl = process.env.MONGODB_URI;
 
 const connectDB = async () => {
  await mongoose.connect(
-  
+  mongodbUrl
  )
 }
 
