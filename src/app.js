740d6fab7fab6bd47b454a3fa88683b2fdea21f9
swapnil0076 <swapnildhiman910@gmail.com>
2026-01-01 19:44:28 +0530

    API New Changes

--- src/app.js
+++ src/app.js
@@ -8,25 +8,78 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/SignUp' , async (req , res) => {
- 
-const userDb = {
-  firstName : req.body.firstName,
-  lastName : req.body.lastName,
-  email : req.body.email,
-  password : req.body.password,
-  age : req.body.age,
-  gender : req.body.gender
-}
 
 
-
-const user = new User(userDb);
+const user = new User(req.body);
 
 await user.save().then(() => {
-  res.status(201).send({ message: "User registered successfully" });
-}).catch((err) => {
-  res.status(500).send({ error: "Failed to register user", details: err });
+      res.status(201).send({ message: "User registered successfully" });
+      }).catch((err) => {
+    res.status(500).send({ error: "Failed to register user", details: err });
+  })
 });
+
+
+app.post('/signIn', async (req , res) =>{
+
+  const {email,password} = req.body;
+  console.log(email);
+
+  await User.findOne({email}).then((u) =>{
+
+    if(password !== u.password){
+      res.status(401).send("Please check your password");
+    }else{
+      res.status(200).send("User signed in successfully");
+    }
+
+    console.log(u.firstName);
+  })
+
+});
+
+
+app.get('/getUser/:userId' , async (req , res) => {
+ const userId = req.params.userId; 
+
+  await User.findById(userId).then((user) => {
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }else {
+      res.status(200).send(user);
+    }
+  }).catch((err) => {
+    res.status(500).send({ error: "Failed to retrieve user", details: err });
+  });
+});
+
+
+app.delete('/user', async (req, res) =>{
+
+const {userId} = req.body;
+  await User.findByIdAndDelete(userId).then((u) =>{
+    res.status(200).send("User deleted successfully");
+  }).catch((err) =>{
+    res.status(500).send({ error: "Failed to delete user", details: err });
+  })
+
+})
+
+app.patch('/user', async (req, res) =>{
+
+const {_id} = req.body.userId;
+const data = req.body;
+
+  const updatedUser = await User.findOneAndUpdate({_id:user_Id},data)
+
+  console.log(data);
+
+  if(!updatedUser){
+    return res.status(404).send({error : "User not found"});
+  }
+
+  res.status(200).send("User updated successfully");
+
 });
 
 
